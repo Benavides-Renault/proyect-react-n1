36de49afa42aa4e4c98e7ecdfc09772a8885c62c
FaniTorres <196740857+estefaniii@users.noreply.github.com>
2025-03-03 14:01:50 -0500

    copying file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import phrases from './phrases.json';
+import { randomItems } from './utils';
+import PhraseCard from './components/PhraseCard'
+import Button from './components/Button'
+import { img1_bg, img2_bg, img3_bg, img4_bg } from './assets/img';
+import FlipContainer from './components/FlipContainer';
+
+
+// import IA_Fortune from './utils/IA_Fortune';
+
+const imgs_bg = [img1_bg, img2_bg, img3_bg, img4_bg]
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [phrase, setPhrases] = useState(randomItems(phrases));
+	const [img_bg, setImg_bg] = useState(randomItems(imgs_bg))
+	const [flip, setFlip] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	// const changePhrase = async () => {
+	// 	setFlip(true);
+  //   setTimeout(async () => {
+	// 		const fortune = await IA_Fortune();
+  //     setPhrases({ phrase: fortune, author: 'Diosa Fortuna' });
+  //     setImg_bg(randomItems(imgs_bg));
+  //   }, 500);
+  //   setTimeout(() => setFlip(false), 250);
+  // };
+
+	const changePhrase = () => {
+		setFlip(true);
+    setTimeout(() => {
+      setPhrases(randomItems(phrases));
+      setImg_bg(randomItems(imgs_bg));
+    }, 0);
+    setTimeout(() => setFlip(false), 0);
+  };
+
+	return (
+		<FlipContainer flip={flip}>
+			<div className='container' style={{backgroundImage: `url('${img_bg}')`}}>
+					<h1>Galleta de la Fortuna</h1>
+					
+					<PhraseCard
+						phrase={phrase.phrase}
+						author={phrase.author}
+					/>
+					
+					<Button handlePhrase={changePhrase}>
+						Prueba tu fortuna
+					</Button>
+			</div>
+		</FlipContainer>
+	);
 }
 
-export default App
+export default App;
